3118aaa67f74e656fef23d13c461e5a3b115f9c3
swordcube <swordsoftarchive@gmail.com>
2023-09-22 16:25:21 -0500

    slightly breaking changes
    just replace all cpp.Pointer.addressOf(arg) when calling the function with arg, it'll handle it on it's own now

--- src/dr/Wav.hx
+++ src/dr/Wav.hx
@@ -1,20 +1,26 @@
 package dr;
 
-@:keep
-@:include("vendor/include/dr_wav.h")
-@:buildXml("<include name=\"${haxelib:hxdr_wav}/include.xml\"/>")
 
 // WARNING: This will not have every feature dr_wav has to offer!
 //
 // Make an issue explaining what feature you want implemented over on GitHub!
 // https://github.com/swordcube/hxdr_wav/issues
 
+@:include("vendor/include/dr_wav.h")
+@:buildXml("<include name=\"${haxelib:hxdr_wav}/include.xml\"/>")
+@:unreflective
 extern class Wav {
+	// can't use RawPointers because those seem to break :[
 	@:native('drwav_open_file_and_read_pcm_frames_s16')
-	public extern static function openFileAndReadPCMFramesShort16(fileName:cpp.ConstCharStar, channels:cpp.Pointer<cpp.UInt32>, sampleRate:cpp.Pointer<cpp.UInt32>, totalFrameCount:cpp.Pointer<cpp.UInt64>, allocationCallbacks:cpp.ConstPointer<DrWavAllocationCallbacks>):cpp.Pointer<cpp.Int16>;
+	@:noCompletion
+	private static function __openFileAndReadPCMFramesShort16(fileName:cpp.ConstCharStar, channels:cpp.RawPointer<cpp.UInt32>, sampleRate:cpp.RawPointer<cpp.UInt32>, totalFrameCount:cpp.RawPointer<cpp.UInt64>, allocationCallbacks:cpp.ConstPointer<DrWavAllocationCallbacks>):cpp.RawPointer<cpp.Int16>;
+
+	static inline function openFileAndReadPCMFramesShort16(fileName:cpp.ConstCharStar, channels:cpp.UInt32, sampleRate:cpp.UInt32, totalFrameCount:cpp.UInt64, allocationCallbacks:cpp.ConstPointer<DrWavAllocationCallbacks>):cpp.RawPointer<cpp.Int16> {
+		return __openFileAndReadPCMFramesShort16(fileName, cpp.RawPointer.addressOf(channels), cpp.RawPointer.addressOf(sampleRate), cpp.RawPointer.addressOf(totalFrameCount), allocationCallbacks);
+	}
 
 	@:native('drwav_free')
-	public extern static function free(data:cpp.Pointer<cpp.Void>, allocationCallbacks:cpp.ConstPointer<DrWavAllocationCallbacks>):Void;
+	public extern static function free(data:cpp.RawPointer<cpp.Void>, allocationCallbacks:cpp.ConstPointer<DrWavAllocationCallbacks>):Void;
 }
 
 @:keep
@@ -23,9 +29,9 @@ extern class Wav {
 @:native('drwav_allocation_callbacks')
 @:structAccess
 extern class DrWavAllocationCallbacks {
-	var userData:cpp.Pointer<cpp.Void>;
+	var userData:cpp.RawPointer<cpp.Void>;
 
-	var onMalloc:cpp.Pointer<cpp.Void>;
-	var onRealloc:cpp.Pointer<cpp.Void>;
-	var onFree:cpp.Pointer<cpp.Void>;
+	var onMalloc:cpp.RawPointer<cpp.Void>;
+	var onRealloc:cpp.RawPointer<cpp.Void>;
+	var onFree:cpp.RawPointer<cpp.Void>;
 }
