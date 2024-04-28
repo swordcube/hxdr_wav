704edb5633111bcce9bd6d620974633b5dbb8fa0
swordcube <swordsoftarchive@gmail.com>
2024-04-27 22:07:23 -0500

    tesitng .. .

--- src/dr/WAV.hx
+++ src/dr/WAV.hx
@@ -10,17 +10,15 @@ package dr;
 @:buildXml("<include name=\"${haxelib:hxdr_wav}/include.xml\"/>")
 @:unreflective
 extern class WAV {
-	// can't use RawPointers because those seem to break :[
 	@:native('drwav_open_file_and_read_pcm_frames_s16')
-	@:noCompletion
-	private static function __openFileAndReadPCMFramesShort16(fileName:cpp.ConstCharStar, channels:cpp.RawPointer<cpp.UInt32>, sampleRate:cpp.RawPointer<cpp.UInt32>, totalFrameCount:cpp.RawPointer<cpp.UInt64>, allocationCallbacks:cpp.ConstPointer<DrWavAllocationCallbacks>):cpp.RawPointer<cpp.Int16>;
-
-	static inline function openFileAndReadPCMFramesShort16(fileName:cpp.ConstCharStar, channels:cpp.UInt32, sampleRate:cpp.UInt32, totalFrameCount:cpp.UInt64, allocationCallbacks:cpp.ConstPointer<DrWavAllocationCallbacks>):cpp.RawPointer<cpp.Int16> {
-		return __openFileAndReadPCMFramesShort16(fileName, cpp.RawPointer.addressOf(channels), cpp.RawPointer.addressOf(sampleRate), cpp.RawPointer.addressOf(totalFrameCount), allocationCallbacks);
+	static inline function openFileAndReadPCMFramesShort16(fileName:cpp.ConstCharStar, channels:cpp.UInt32, sampleRate:cpp.UInt32, totalFrameCount:DrWAVUInt64, allocationCallbacks:cpp.RawConstPointer<DrWavAllocationCallbacks>):cpp.RawPointer<cpp.Int16> {
+		return untyped __cpp__("drwav_open_file_and_read_pcm_frames_s16({0}, {1}, {2}, {3}, {4})", fileName, cpp.RawPointer.addressOf(channels), cpp.RawPointer.addressOf(sampleRate), cpp.RawPointer.addressOf(totalFrameCount), allocationCallbacks);
 	}
 
 	@:native('drwav_free')
-	public extern static function free(data:cpp.RawPointer<cpp.Void>, allocationCallbacks:cpp.ConstPointer<DrWavAllocationCallbacks>):Void;
+	static inline function free(data:Any, allocationCallbacks:cpp.RawConstPointer<DrWavAllocationCallbacks>):Void {
+        untyped __cpp__('drwav_free((void*){0}, {1})', data, allocationCallbacks);
+    }
 }
 
 @:keep
@@ -35,3 +33,9 @@ extern class DrWavAllocationCallbacks {
 	var onRealloc:cpp.RawPointer<cpp.Void>;
 	var onFree:cpp.RawPointer<cpp.Void>;
 }
+
+@:include("vendor/include/dr_wav.h")
+@:buildXml("<include name=\"${haxelib:hxdr_wav}/include.xml\"/>")
+@:native("drwav_uint64")
+@:scalar @:coreType @:notNull
+extern abstract DrWAVUInt64 from Int to Int {}
